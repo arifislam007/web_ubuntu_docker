f1194b5e6e503c9eca9fcc6e32df758312a2c565
MD. AREFUL ISLAM <32135229+arifislam007@users.noreply.github.com>
2023-06-15 19:02:25 +0600

    Update run_bash.sh

--- run_bash.sh
+++ run_bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir data
+mkdir /data
 docker build -t u_nginx:v1 .
 docker run -itd --name ucon2 -p 86:80 --mount type=bind,source=/data,destination=/var/www/html u_nginx:v1
 cp ./index.html /data/
